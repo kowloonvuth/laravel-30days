7ea2752597e8206de885378dac38180e954176b3
Rany Virakvuth <ranyvirakvuth@gmail.com>
2025-04-06 10:27:39 +0700

    Baby steps

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,5 @@
+@props(['active' => true])
+
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} block rounded-md px-2 py-2 text-sm font-medium text-white"
+    aria-current="{{ $active ? 'page' : 'false'}}"
+    {{$attributes}}>{{$slot}}</a>
